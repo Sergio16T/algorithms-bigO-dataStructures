e96cd5b4b4723b1e8e3c2b7911fc30de11dd56d6
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2020-11-18 01:03:45 -0600

    Added dataStructures directory

---
+++ dataStructures/PriorityQueue.js
@@ -0,0 +1,49 @@
+//FIFO
+class QueueElement {
+    constructor(element, priority) {
+        this.element = element;
+        this.priority = priority
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.items = [];
+    };
+    enQueue = (element, priority) => {
+        let item = new QueueElement(element, priority);
+
+        for(let i = 0; i < this.items.length; i++) {
+            if(this.items[i].priority > item.priority) {
+                this.items.splice(i, 0, item);
+                return;
+            }
+        }
+        this.items.push(item);
+    }
+    deQueue = () => {
+        return this.items.shift();
+    }
+    firstItem = () => {
+        return this.items[0];
+    }
+    isEmpty = () => {
+        return this.items.length == 0;
+    }
+    clear = () => {
+        this.items = [];
+    }
+    size = () => {
+        return this.items.length;
+    }
+    print = () => {
+        console.log(this.items);
+    }
+}
+
+const queue =  new PriorityQueue();
+queue.enQueue(5, 1);
+queue.enQueue(6, 3);
+queue.enQueue(2, 2);
+queue.enQueue(4, 4);
+queue.print();
---
+++ dataStructures/Stack.js
@@ -0,0 +1,35 @@
+// LIFO
+class Stack {
+    constructor() {
+        this.items = [];
+    };
+
+    pop = () => {
+        return this.items.pop();
+    }
+    push = (element) => {
+        return this.items.push(element);
+    }
+    peek = () => {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty = () => {
+        return this.items.length == 0;
+    }
+    size = () => {
+        return this.items.length;
+    }
+    clear = () => {
+        this.items = [];
+    }
+    print = () => {
+        console.log(this.items.toString());
+    }
+    toString = () => {
+        return this.items.toString();
+    }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.print();
