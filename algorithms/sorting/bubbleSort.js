1d3f2c2f8c59fad85d67284a10616229a4f85439
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2020-12-30 20:08:08 -0600

    Added directory for sorting algorithms

---
+++ algorithms/sorting/bubbleSort.js
@@ -0,0 +1,21 @@
+// Sort the elements in an array. One way to do this is using bubble sort as follows:
+// O(n^2) quadratic time
+
+
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+  return array;
+}
+
+
+let array = [0, 6, 5, 1, 3, 2, 4];
+
+console.log(bubbleSort(array));
