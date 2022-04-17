843690d8a64e6be62bee1b9bb59ed584c73747fb
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2022-04-16 20:23:23 -0500

    ALGO-1: Update tests

--- __tests__/plusOne.test.js
+++ __tests__/plusOne.test.js
@@ -1,12 +1,12 @@
-const plusOne =  require('../algorithms/plusOne'); 
+const plusOne =  require('../algorithms/Arrays&Strings/plusOne');
 
 describe('it tests plusOne solution', () => {
     it('adds 1 to end of array when last digit less than 9', () => {
-        let input = [1,2,3]; 
+        let input = [1,2,3];
         expect(plusOne(input)).toEqual([1,2,4]);
-    }); 
+    });
     it('carries the one with edge case [9, 9]', () => {
-        let input = [9, 9]; 
-        expect(plusOne(input)).toEqual([1, 0, 0]); 
+        let input = [9, 9];
+        expect(plusOne(input)).toEqual([1, 0, 0]);
     });
 });
